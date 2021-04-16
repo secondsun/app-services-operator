e3d45f523dc3251f47905d766d810e0464bc196b
Summers Pittman <secondsun@gmail.com>
2021-04-16 11:13:02 -0400

    fix: hack remove later

--- source/rhoas/src/main/java/com/openshift/cloud/controllers/CloudServicesRequestController.java
+++ source/rhoas/src/main/java/com/openshift/cloud/controllers/CloudServicesRequestController.java
@@ -6,8 +6,15 @@ import com.openshift.cloud.beans.KafkaK8sClients;
 import com.openshift.cloud.utils.InvalidUserInputException;
 import com.openshift.cloud.v1alpha.models.CloudServicesRequest;
 import com.openshift.cloud.v1alpha.models.UserKafka;
+
+import io.fabric8.kubernetes.client.WatcherException;
 import io.javaoperatorsdk.operator.api.*;
+import io.javaoperatorsdk.operator.processing.event.DefaultEventSourceManager;
 import io.javaoperatorsdk.operator.processing.event.EventSourceManager;
+import io.javaoperatorsdk.operator.processing.event.internal.CustomResourceEventSource;
+import io.quarkus.scheduler.Scheduled;
+
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.logging.Logger;
 import javax.inject.Inject;
@@ -25,13 +32,39 @@ public class CloudServicesRequestController
 
   @Inject KafkaApiClient apiClient;
 
+  private EventSourceManager eventSourceManager;
+
   public CloudServicesRequestController() {}
 
   /** @return true if there were changes, false otherwise */
   @Override
   public void init(EventSourceManager eventSourceManager) {
+    this.eventSourceManager = eventSourceManager;
     LOG.info("Init! This is where we would add watches for child resources");
   }
+
+@Scheduled(every = "600s")  
+public void reconnect() {
+  DefaultEventSourceManager x = (DefaultEventSourceManager) this.eventSourceManager;
+  Field f;
+  if (x != null)
+  try {
+    f = x.getClass().getDeclaredField("customResourceEventSource");
+    f.setAccessible(true);
+    CustomResourceEventSource cre = (CustomResourceEventSource) f.get(x);
+    cre.onClose(new WatcherException("Hack Exception"){
+      @Override
+      public boolean isHttpGone() {
+          return true;
+      }
+    });
+  } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+    // TODO Auto-generated catch block
+    e.printStackTrace();
+  }
+  
+
+}
 
   @Override
   void doCreateOrUpdateResource(
